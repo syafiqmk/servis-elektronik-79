7955cb6bd934e9032e00b37b83da1d29af02f8d4
syafiqmk <syafiqmuh.k@gmail.com>
2023-05-20 16:07:11 +0800

    Update Category index view

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,3 +3,34 @@
 @section('title')
     {{ $title }}
 @endsection
+
+@section('content')
+    
+    {{-- Table --}}
+    <table class="table">
+        <thead>
+            <th>No</th>
+            <th>Category</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            @if ($categories->count() == 0)
+                <tr>
+                    <td colspan="3" class="text-center">No entries found.</td>
+                </tr>
+            
+            @else
+                @foreach ($categories as $data)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $data->category }}</td>
+                        <td>
+                            <a href="" class="btn btn-success"><span data-feather="edit"></span> Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+
+@endsection
