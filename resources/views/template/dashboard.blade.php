076cb52cff4370a4b9a9f07698d8cfc1684b206c
syafiqmk <syafiqmuh.k@gmail.com>
2023-05-22 10:36:03 +0800

    update dashboard template

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -109,8 +109,16 @@
 
             {{-- content --}}
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                {{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">@yield('title')</h1>
+                </div> --}}
+                <div class="row pt-3 pb-2 mb-3 border-bottom">
+                    <div class="col-md-6 col-sm-12">
+                        <h1 class="h2">@yield('title')</h1>
+                    </div>
+                    <div class="col-md-6 col-sm-12 text-end">
+                        @yield('side_title')
+                    </div>
                 </div>
                 
                 {{-- alerts --}}
