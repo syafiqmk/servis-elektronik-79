a02559faafca5b8e9fd5110721ea5c73b91c0ee5
syafiqmk <syafiqmuh.k@gmail.com>
2023-05-22 23:50:12 +0800

    add link to add new device

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -3,3 +3,7 @@
 @section('title')
     Devices list
 @endsection
+
+@section('side_title')
+    <a href="{{ route('device.create') }}" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Add new device</a>
+@endsection
