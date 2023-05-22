808738fcc36d6037af3779ea1bf67164aa61eee2
syafiqmk <syafiqmuh.k@gmail.com>
2023-05-22 22:58:37 +0800

    add method create to DeviceController

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -17,4 +17,34 @@ class DeviceController extends Controller
             'categories' => $categories
         ]);
     }
+
+    // Submit
+    public function submit(Request $request) {
+        $validate = $request->validate([
+            'image' => 'image|file',
+            'category' => 'required',
+            'phone' => 'numeric',
+            'detail' => 'required'
+        ]);
+
+        if($request->file('image')) {
+            $validate['image'] = $request->file('image')->store('images/device');
+        } else {
+            $validate['image'] = NULL;
+        }
+
+        $create = Device::create([
+            'phone_number' => $validate['phone'],
+            'detail' => $validate['detail'],
+            'image' => $validate['image'],
+            'status' => 'Baru',
+            'category_id' => $validate['category'],
+        ]);
+
+        if($create) {
+            return redirect()->route('device.index')->with('success', 'Device added succesfully!');
+        } else {
+            return redirect()->route('device.index')->with('danger', 'Fail to add new device!');
+        }
+    }
 }
