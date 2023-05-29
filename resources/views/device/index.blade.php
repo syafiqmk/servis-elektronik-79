7429f5061ac47522fd2516e7f0296c2c8a593700
syafiqmk <syafiqmuh.k@gmail.com>
2023-05-29 15:57:25 +0800

    update device index view

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -13,6 +13,7 @@
 
 @section('content')
 
+    <h5>Filters</h5>
     {{-- search --}}
     <form action="{{ route('device.index') }}" method="get" class="mb-2">
 
@@ -66,67 +67,69 @@
     </form>
     {{-- end of search --}}
 
-    <div class="container">
-        @if ($devices->count() != 0)
-        <div class="row">
-            @foreach ($devices as $data)
-            <div class="col-md-4 col-sm-6 mb-3">
-                <div class="card h-100">
-                    {{-- card header --}}
-                    <div class="card-header">
-                        {{-- status & category --}}
-                        <b>{{ $data->category->category }}</b>
-                        @switch($data->status)
-                            @case('Baru')
-                                <span class="badge bg-primary">{{ $data->status }}</span>
-                                @break
-                            @case('Proses')
-                                <span class="badge bg-warning">{{ $data->status }}</span>
-                                @break
-                            @case('Belum Diambil')
-                                <span class="badge bg-info">{{ $data->status }}</span>
-                                @break
-                            @case('Sudah Diambil')
-                                <span class="badge bg-success">{{ $data->status }}</span>
-                                @break
-                            @case('Batal')
-                                <span class="badge bg-danger">{{ $data->status }}</span>
-                                @break
-                        @endswitch
-                        {{-- end of status and category --}}
-                    </div>
-                    {{-- end of car header --}}
-
-                    {{-- image --}}
-                    <img src="{{ (empty($data->image)) ? asset('image').'/No-Image.png' : asset('image/device').'/'.$data->image }}" class="h-50">
-                    {{-- end of image --}}
-
-                    {{-- card body --}}
-                    <div class="card-body">
-                        {{-- time created --}}
-                            <p class="text-end">{{ $data->created_at->diffForHumans() }}</p>
-                        {{-- end of time created --}}
-
-                        {{-- price --}}
-                        @if (!empty($data->price))
-                            <p>Rp. {{ number_format($data->price, 2, ',', '.') }}</p>
-                        @endif
-                        {{-- end of price --}}
-
-                        {{-- link --}}
-                        <div class="text-end">
-                            <a href="{{ route('device.show', $data->id) }}" class="btn btn-primary"><i class="fa-solid fa-book"></i> Detail</a>
-                        </div>
-                        {{-- end of link --}}
-                    </div>
-                    {{-- end of card body --}}
+    {{-- device list --}}
+    <h5>Devices</h5>
+    @if ($devices->count() != 0)
+    <div class="row">
+        @foreach ($devices as $data)
+        <div class="col-md-4 col-sm-6 mb-3">
+            <div class="card h-75">
+                {{-- card header --}}
+                <div class="card-header">
+                    {{-- status & category --}}
+                    <b>{{ $data->category->category }}</b>
+                    @switch($data->status)
+                        @case('Baru')
+                            <span class="badge bg-primary">{{ $data->status }}</span>
+                            @break
+                        @case('Proses')
+                            <span class="badge bg-warning">{{ $data->status }}</span>
+                            @break
+                        @case('Belum Diambil')
+                            <span class="badge bg-info">{{ $data->status }}</span>
+                            @break
+                        @case('Sudah Diambil')
+                            <span class="badge bg-success">{{ $data->status }}</span>
+                            @break
+                        @case('Batal')
+                            <span class="badge bg-danger">{{ $data->status }}</span>
+                            @break
+                    @endswitch
+                    {{-- end of status and category --}}
                 </div>
+                {{-- end of car header --}}
+
+                {{-- image --}}
+                <img src="{{ (empty($data->image)) ? asset('image').'/No-Image.png' : asset('image/device').'/'.$data->image }}" class="" height="45%">
+                {{-- end of image --}}
+
+                {{-- card body --}}
+                <div class="card-body">
+                    {{-- time created --}}
+                        <p class="text-end">{{ $data->created_at->diffForHumans() }}</p>
+                    {{-- end of time created --}}
+
+                    {{-- price --}}
+                    @if (!empty($data->price))
+                        <p>Rp. {{ number_format($data->price, 2, ',', '.') }}</p>
+                    @endif
+                    {{-- end of price --}}
+
+                    {{-- link --}}
+                    <div class="text-end">
+                        <a href="{{ route('device.show', $data->id) }}" class="btn btn-primary"><i class="fa-solid fa-book"></i> Detail</a>
+                    </div>
+                    {{-- end of link --}}
+                </div>
+                {{-- end of card body --}}
             </div>
-            @endforeach
         </div>
-        {{ $devices->withQueryString()->links() }}
-        @else
-            <p>No entries found!</p>
-        @endif
+        @endforeach
     </div>
+    {{ $devices->withQueryString()->links() }}
+    @else
+        <p>No entries found!</p>
+    @endif
+    {{-- end of device list --}}
+
 @endsection
