61e0fbae7591c19531d00ed261f09f0a45123898
syafiqmk <syafiqmuh.k@gmail.com>
2023-06-06 14:06:21 +0800

    fix sizing bug in map iframe in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
             <div class="h2">Kontak</div>
             <div class="row">
                 <div class="col-md-6 col-sm-12">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d498.706588617801!2d117.15186875313519!3d-0.5220446959531964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df67f3dca72dcc3%3A0xe24cc287cb610353!2sSarana%20Mitra%20Elektrindo!5e0!3m2!1sid!2sid!4v1685946055881!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d498.706588617801!2d117.15186875313519!3d-0.5220446959531964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df67f3dca72dcc3%3A0xe24cc287cb610353!2sSarana%20Mitra%20Elektrindo!5e0!3m2!1sid!2sid!4v1685946055881!5m2!1sid!2sid" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-md-6 col-sm-12">
                     <div class="mb-3">
